0c644409821316eb3f15036dbffa39c93450e7a4
Thomas de Heus <thomas.deheus@aurecongroup.com>
2025-12-24 16:07:07 +1030

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+st.write("This is new text")
